2930d7c88948b3b63112416e421b053f2c6e5482
Andrew <andrewdblum@gmail.com>
2019-07-13 15:32:09 -0700

    intersection of two linked lists

--- practice.js
+++ practice.js
@@ -671,4 +671,56 @@ var findUnsortedSubarray = function(nums) {
 
 
 
+// leet 160 Linked List intersection 
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+  let aLength = 0;
+  let bLength = 0; 
+  let a = headA;
+  let b = headB;
+  
+  while(a) {
+      aLength += 1
+      a = a.next
+  }
+  while(b) {
+      bLength += 1
+      b = b.next
+  }
+
+  
+  let offset; 
+  if (aLength > bLength) {
+      offset = aLength - bLength; 
+      for (let i = offset; offset > 0; offset--) {
+          headA = headA.next;
+      }
+  }
+  if (aLength < bLength) {
+      offset = bLength - aLength;
+      for (let i = offset; offset > 0; offset--) {
+          headB = headB.next;
+      }
+  }
+  
+  while(headA && headB) {
+      if (headA === headB) {
+          return headA
+      }
+      headA = headA.next;
+      headB = headB.next;
+  }
+};
