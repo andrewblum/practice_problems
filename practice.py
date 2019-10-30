de9ee5281e94744a5b65622c05f94e76a663f05a
Andrew <andrewdblum@gmail.com>
2019-10-30 16:18:29 -0700

    plain recursive count change

--- practice.py
+++ practice.py
@@ -99,8 +99,21 @@ def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
     return x and y
 
 
+# recursion without memo
+def coinChange(self, coins, amount):
+    if amount < 0: return -1
+    if amount == 0: return 0
+    results = []
+    for coin in coins: 
+        pos = self.helper(coins, amount - coin)
+        if pos > -1:
+            results.append(pos + 1)
+    if results:
+        return min(results)
+    return -1
 
-def coinChange(self, coins: List[int], amount: int) -> int:
+# recursion with memo
+def coinChange2(self, coins: List[int], amount: int) -> int:
     memo = {}
     return self.helper(coins, amount, memo)
     
