9a2c011749846a4e8be296c0a9961e81590b38a1
Andrew Blum <andrewdblum@gmail.com>
2020-02-04 20:38:32 -0800

    merge trees

--- practice.py
+++ practice.py
@@ -904,3 +904,13 @@ def helper(oldx, oldy, board, cell):
     if cell == 1 and neighbors == 2:
         return 1
     return 0
+
+def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:  
+    if not t2 and not t1: return
+    if t2 and t1:
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
+    else:
+        return t1 or t2
