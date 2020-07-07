9178e37b039d2829a85a148a3fa7ef1bc3592fc9
Andrew Blum <andrewdblum@gmail.com>
2020-07-07 02:51:22 -0700

    plus one

--- practice.py
+++ practice.py
@@ -2615,3 +2615,12 @@ def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
 #             cells = [0] + copy[1:-1] + [0]
         cells = [0] + [int(cells[i-1] == cells[i+1]) for i in range(1,7)] + [0]
     return cells
+
+def plusOne(self, digits: List[int]) -> List[int]:
+    for i in range(len(digits)-1, -1, -1):
+        if digits[i] < 9:
+            digits[i] += 1
+            return digits 
+        else: 
+            digits[i] = 0
+    return [1] + digits
